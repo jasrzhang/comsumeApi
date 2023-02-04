c10c1952ae6de53436207d0a392b817c36049a96
jasrzhang <zhangshurui1989@gmail.com>
2023-02-04 14:31:21 +1030

    Comsuming SOAP API Using WSDL Generated Files

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # comsumeApi
 Demostrate how to consume REST and SOAP API
 
-Main Branch demostrates consuming of REST API
 SOAP branch demostrates consuming of SOAP API
+
+Includes WSDL file, copy the WSDL xml file from 
+https://www.dataaccess.com/webservicesserver/numberconversion.wso?WSDL
+
+The WSDL file defines the operations.
+
+Code Generation:
+
+1. add Maven POM file for Jaxb Maven Plugins and pointing to the wsdl file.
+2. Click on Excute Maven Goal, and generate sources
+3. Check the generated file under target/generated-sources/xjc
--- consuming/src/main/java/com/example/consuming/ConsumingApplication.java
+++ consuming/src/main/java/com/example/consuming/ConsumingApplication.java
@@ -1,37 +1,37 @@
 package com.example.consuming;
 
-import com.example.consuming.entity.Joke;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.dataaccess.webservicesserver.NumberToWordsResponse;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 @SpringBootApplication
-public class ConsumingApplication {
-
-	private static final Logger log = LoggerFactory.getLogger(ConsumingApplication.class);
+public class ConsumingApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumingApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
+	@Override
+	public void run(String... args) throws Exception {
+		NumberClient numberClient = new NumberClient();
 
-	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		return args -> {
-			Joke joke = restTemplate.getForObject(
-					"https://official-joke-api.appspot.com/random_joke", Joke.class);
-			log.info(joke.toString());
-		};
-	}
+		//create and setup marshaller
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
 
+		//provide location to the ObjectFacory
+		marshaller.setContextPath("com.dataaccess.webservicesserver");
+
+		//add marshaller to the client
+		numberClient.setMarshaller(marshaller);
+		numberClient.setUnmarshaller(marshaller);
+
+		//retrieve the word format of the number
+		NumberToWordsResponse response = numberClient.getWords("3454");
+
+		//print to screen
+		System.out.println("Response is: " + response.getNumberToWordsResult());
+	}
 
 }
